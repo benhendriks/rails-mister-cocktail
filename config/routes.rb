1849aeaa32a361b1ecddd658062c4768e78019c3
Benjamin Steidl <localartpool@gmail.com>
2020-05-09 08:04:41 +0200

    Add the doses method to my file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :cocktails
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :doses
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html :destroy
 end
